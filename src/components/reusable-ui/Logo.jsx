03a03487919156e6a1db3b3cc9ab3e8f1a5e59e9
Valtkf <valentin.vgregorio@gmail.com>
2024-01-25 12:02:20 +0100

    ♻️ F03: images > rename images folder

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export default function Logo() {
     return (
-        <LogoStyled>Logo</LogoStyled>
+        <LogoStyled>LOGO CRAZEE BURGER</LogoStyled>
     )
 }
 
