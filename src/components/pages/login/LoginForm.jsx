4476485137ada81cd886ff6adf62b03f114e4f68
Valtkf <valentin.vgregorio@gmail.com>
2024-01-26 15:13:44 +0100

    💄 F03: LOGIN > LoginForm > inpt > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom"
 import styled from 'styled-components'
+import { BsPersonCircle } from "react-icons/bs";
 
 export default function LoginForm() {      
     const [inputValue, setInputValue] = useState("")
@@ -22,13 +23,16 @@ export default function LoginForm() {
     <h1>Bienvenue chez nous !</h1>
     <hr />
     <h2>Connectez-vous</h2>
-        <input 
-            value={inputValue} 
-            onChange={handleChange} 
-            type="text" 
-            placeholder="Entrez votre prénom" 
-            required
-        />
+        <div className="input-with-icon">
+        <BsPersonCircle className="icon"/>
+            <input
+                value={inputValue}
+                onChange={handleChange}
+                type="text"
+                placeholder="Entrez votre prénom"
+                required
+            />
+        </div>
         <button>Accéder à mon espace</button>
     </LoginFormStyled>
     )
@@ -59,5 +63,32 @@ const LoginFormStyled = styled.div`
         margin: 20px 10px 10px;
         color: white;
         font-size: 36px;
+    }
+
+    .input-with-icon{
+        background-color: #fff;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        padding: 18px 24px;
+        margin: 18px 0;
+        
+
+        .icon{
+            font-size: 15px;
+            margin-right: 8px;
+            color: #93a2b1;
+        }
+
+        input{
+            border: none;
+            font-size: 15px;
+            color: #17161a;
+        }
+
+        &::placeholder{
+            background: white;
+            color: lightgrey;
+        }
     }
 `;
