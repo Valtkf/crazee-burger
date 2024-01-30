bd3e7e380a9a6227092a6d27550342dd11605fe4
Valtkf <valentin.vgregorio@gmail.com>
2024-01-30 10:55:32 +0100

    ♻️ F03: LOGIN > LoginForm > Create PrimaryButton

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -28,6 +28,7 @@ const InputStyled = styled.div`
             border: none;
             font-size: 15px;
             color: #17161a;
+            width: 100%;
         }
 
         &::placeholder{
