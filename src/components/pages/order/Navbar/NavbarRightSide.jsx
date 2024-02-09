668468ece8a721e007b41688b74daf99b69fe164
Valtkf <valentin.vgregorio@gmail.com>
2024-02-09 12:00:44 +0100

    ✨ F06: ORDER > NavbarRightSide > add toggle button

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
+
 
 
 export default function NavbarRghtSide({username}) {
     return (
         <NavbarRghtSideStyled className="right-side">
-            
+            <ToggleButton />
             <Profile username={username}/>
         </NavbarRghtSideStyled>
     )
