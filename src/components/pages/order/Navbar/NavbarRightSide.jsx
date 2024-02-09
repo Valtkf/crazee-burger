524d7bcbfcc805b66957c504ad0d3316203b3339
Valtkf <valentin.vgregorio@gmail.com>
2024-02-09 11:52:35 +0100

    🍱 F05: Add assets

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+
+
 export default function NavbarRghtSide({username}) {
     return (
         <NavbarRghtSideStyled className="right-side">
-            {/*<div className="admin-button">Admin button</div>*/}
+            
             <Profile username={username}/>
         </NavbarRghtSideStyled>
     )
