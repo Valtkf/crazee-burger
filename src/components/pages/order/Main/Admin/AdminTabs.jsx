9bfeb614e7ec7840dd5d106787987f2f401c5324
Valtkf <valentin.vgregorio@gmail.com>
2024-02-19 16:04:57 +0100

    ✨ F07: ADMIN > Tab > Add label "Modifier un produit"

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Tab from '../../../../reusable-ui/Tab';
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
 import { theme } from '../../../../theme';
 
 export default function AdminTabs({isCollapsed, setIsCollapsed}) {
@@ -17,6 +18,12 @@ export default function AdminTabs({isCollapsed, setIsCollapsed}) {
                 <Tab
                     label="Ajouter un produit"
                     Icon={<AiOutlinePlus />}
+                    onClick={() => setIsCollapsed(!isCollapsed)}
+                    className={isCollapsed ? "is-active" : ""}
+                />
+                <Tab
+                    label="Modifier un produit"
+                    Icon={<MdModeEditOutline />}
                     onClick={() => setIsCollapsed(!isCollapsed)}
                     className={isCollapsed ? "is-active" : ""}
                 />
