40ae7100691d67f6da32fc690e618e7bf9e549c9
Valtkf <valentin.vgregorio@gmail.com>
2024-02-20 16:36:57 +0100

    ♻️ F07: ADMIN > add function getTabSelected

--- src/components/pages/order/Main/Admin/getTabsConfig.jsx
+++ src/components/pages/order/Main/Admin/getTabsConfig.jsx
@@ -6,12 +6,13 @@ export const getTabsConfig = (currentTabSelected) => [
         index: "add",
         label: "Ajouter un produit",
         Icon: <AiOutlinePlus />,
-        className: currentTabSelected === "add" ? "is-active" : "",
     },
     {
         index: "edit",
         label: "Modifier un produit",
         Icon: <MdModeEditOutline />,
-        className: currentTabSelected === "edit" ? "is-active" : "",
     },
 ]
+
+export const getTabSelected = (tabs, currentTabSelected) => 
+    tabs.find((tab) => tab.index === currentTabSelected)
