488fa769c3e9d5eddfbf3c80efa32e318a8b9e41
Valtkf <valentin.vgregorio@gmail.com>
2024-02-27 16:04:11 +0100

    💄 F08: ADMIN > addForm> add preview-image style

--- src/components/pages/order/Admin-Panel/AddForm.jsx
+++ src/components/pages/order/Admin-Panel/AddForm.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import OrderContext from "../../../../context/OrderContext";
 import { useContext, useState } from "react";
 import { FiCheck } from "react-icons/fi"
+import { theme } from "../../../theme/index.jsx";
 
 
 const EMPTY_PRODUCT = {
@@ -46,7 +47,7 @@ export default function AddForm() {
                 {newProduct.imageSource ? (
                     <img src={newProduct.imageSource} alt={newProduct.title} />
                 ) : (
-                    <div>Aucune image</div>
+                    <div className="empty-image">Aucune image</div>
                     )}
             </div>
             <div className="input-fields">
@@ -86,7 +87,6 @@ export default function AddForm() {
 }
 
 const AddFormStyled = styled.form`
-    border: 2px solid black;
     display: grid;
     grid-template-columns: 1fr 3fr;
     grid-template-rows: repeat(4, 1fr);
@@ -98,6 +98,8 @@ const AddFormStyled = styled.form`
 
     height: 100%;
     width: 70%;
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
 
     .image-preview{
         grid-area: 1 / 1 / 4 / 2;
@@ -110,6 +112,18 @@ const AddFormStyled = styled.form`
             height: 100%;
             object-fit: contain;
             object-position: center;
+        }
+
+        .empty-image{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid ${theme.colors.greyLight};
+            line-height: 1.5;
+            color: ${theme.colors.greySemiDark};
+            border-radius: ${theme.borderRadius.round};
         }
     }
     .input-fields{
