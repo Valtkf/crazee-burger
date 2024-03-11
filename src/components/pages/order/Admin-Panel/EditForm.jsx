d9c5e7823f9f597c4e2bd70a8fff4afb5098614e
Valtkf <valentin.vgregorio@gmail.com>
2024-03-11 11:09:03 +0100

    🚧  F09: ADMIN > EditForm > Retrieve info of card clicked on with form from AddForm

--- src/components/pages/order/Admin-Panel/EditForm.jsx
+++ src/components/pages/order/Admin-Panel/EditForm.jsx
@@ -1,13 +1,77 @@
-import { useContext } from "react";
-import HintMessage from "./HintMessage";
+import { useContext, useState } from "react";
 import OrderContext from "../../../../context/OrderContext";
+import styled from "styled-components";
+import ImagePreview from "./ImagePreview";
+import { getInputTextsConfig } from "./inputTextConfig.jsx";
+import { EMPTY_PRODUCT } from "../../../../enums/product.jsx";
+import TextInput from "../../../reusable-ui/TextInput";
+
 
 export default function EditForm() {
     const { productSelected } = useContext(OrderContext)
+    const [productBeingEdited, setProductBeingEdited] = useState(EMPTY_PRODUCT)
+
+    const inputTexts = getInputTextsConfig(productSelected)
+
+    const handleChange = (event) => { 
+        const {name, value} = event.target
+        setProductBeingEdited({
+            ...productBeingEdited,
+            [name]: value,
+        })
+    }
+
     return (
-        <div>
-            <HintMessage />
-            <span>{productSelected && productSelected.title}</span>
-        </div>
+        <EditFormStyled >
+            <ImagePreview imageSource={productSelected.imageSource} title={productBeingEdited.title} />
+            <div className="input-fields">
+                {inputTexts.map((input) => (
+                    <TextInput 
+                        name={input.name}
+                        value={input.value}  
+                        placeholder={input.placeholder} 
+                        onChange={handleChange}
+                        Icon={input.Icon}
+                        version="minimalist"
+                    />
+                ))}
+                
+            </div>
+            <div className="submit"></div>
+        </EditFormStyled>
     )
 }
+
+const EditFormStyled = styled.form`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-areas:
+        "image-preview   input-fields"
+        "image-preview   input-fields"
+        "image-preview   input-fields"
+        ".               submit-button";
+
+    height: 100%;
+    width: 70%;
+    grid-column-gap: 20px;
+    grid-row-gap: 8px;
+
+    
+    .input-fields{
+        grid-area: 1 / 2 / 4 / -2;
+        display: grid;
+        grid-row-gap: 8px;
+    }
+    .submit {
+        grid-area: 4 / -2 / -1 / -1;
+        display: flex;
+        align-items: center;
+        
+        .submit-button{
+            width: 50%;
+            cursor: pointer;
+        }
+        
+    }
+`;
