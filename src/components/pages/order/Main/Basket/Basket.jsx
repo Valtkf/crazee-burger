8915fb6c477ba19ced0804becd4e63931e2a6be2
Valtkf <valentin.vgregorio@gmail.com>
2024-04-08 15:06:24 +0200

    ♻️ F10: BASKET > Add Footer

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components"
-import { theme } from "../../../../theme"
 import Total from "./Total"
 import { formatPrice } from "../../../../../utils/maths.jsx"
 import Footer from "./Footer.jsx"
+import BasketBody from "./BasketBody.jsx"
 
 
 
@@ -10,7 +10,7 @@ export default function Basket() {
     return ( 
         <BasketStyled>    
             <Total amountToPay={formatPrice(0)}/>
-                <div className="body">Body</div>
+            <BasketBody />
             <Footer />
         </BasketStyled>
     )
@@ -20,15 +20,4 @@ const BasketStyled = styled.div`
     background: pink;
     display: flex;
     flex-direction: column;
-    
-    .body{
-        background: blue;
-        flex: 1;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: ${theme.colors.background_white};
-        color: ${theme.colors.greyBlue};
-        box-shadow: ${theme.shadows.basket};
-    }
 `
