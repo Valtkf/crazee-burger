0a70f202c9d955e4a4842620ce8bf57681e6819e
Valtkf <valentin.vgregorio@gmail.com>
2024-05-03 16:01:27 +0200

    🚧   F13: BASKET > Firestore > Add "C" from Crud

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react"
 import { fakeBasket } from "../fakeData/fakeBasket.jsx"
 import { deepClone, findObjectById, findIndexById, removeObjectById } from "../utils/array.jsx"
+import { setLocalStorage } from "../utils/window.jsx"
 
 export const useBasket = () => { 
     const [basket, setBasket] = useState(fakeBasket.EMPTY)
     
-    const handleAddToBasket = (idProductToAdd) => { 
+    const handleAddToBasket = (idProductToAdd, username) => { 
         const basketCopy = deepClone(basket)
         const productAlreadyInBasket = findObjectById(idProductToAdd, basketCopy)
 
         if(productAlreadyInBasket){
-            incrementProductAlreadyInBasket(idProductToAdd, basketCopy)
+            incrementProductAlreadyInBasket(idProductToAdd, basketCopy, username)
             return
         }
 
         createNewBasketProduct(idProductToAdd, basketCopy, setBasket)
     }
 
-            const incrementProductAlreadyInBasket = (idProductToAdd, basketCopy) => {
+            const incrementProductAlreadyInBasket = (idProductToAdd, basketCopy, username) => {
                 const indexOfBasketProductToIncrement = findIndexById(idProductToAdd, basketCopy)
                 basketCopy[indexOfBasketProductToIncrement].quantity += 1
                 setBasket(basketCopy)
+                setLocalStorage(username, basketCopy)
             }
 
-            const createNewBasketProduct = (idProductToAdd, basketCopy, setBasket) => {
+            const createNewBasketProduct = (idProductToAdd, basketCopy, setBasket, username) => {
                 const newBasketProduct = { id: idProductToAdd, quantity: 1 }
                 const newBasket = [newBasketProduct, ...basketCopy]
-            
                 setBasket(newBasket)
+                setLocalStorage(username, newBasket)
             }
 
         const handleDeleteBasketProduct = (idBasketProduct) => { 
